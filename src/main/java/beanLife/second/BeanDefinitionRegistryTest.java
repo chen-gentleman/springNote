d9bf408d2bdbd9e5eff8db62745f4a1e06773392
chenxingchang_wotian <chenxingchang@wotian.com>
2022-04-26 18:00:38 +0800

    第2次提交

--- src/main/java/beanLife/second/BeanDefinitionRegistryTest.java
+++ src/main/java/beanLife/second/BeanDefinitionRegistryTest.java
@@ -1,6 +1,9 @@
 package beanLife.second;
 
 import org.junit.Test;
+import org.springframework.beans.factory.support.BeanDefinitionBuilder;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.support.GenericBeanDefinition;
 
 /**
  * BeanDefinitionRegistry 案例
@@ -10,6 +13,19 @@ import org.junit.Test;
 public class BeanDefinitionRegistryTest {
     @Test
     public void test1(){
+        //创建一个bean工厂，这个默认实现了BeanDefinitionRegistry接口，所以也是一个bean注册器
+        DefaultListableBeanFactory factory = new DefaultListableBeanFactory();
+        //定义一个bean
+        GenericBeanDefinition nameBdf = new GenericBeanDefinition();
+        nameBdf.setBeanClass(String.class);
+        nameBdf.getConstructorArgumentValues().addIndexedArgumentValue(0,"陈杏昌");
+        //将bean注册到容器中
+        factory.registerBeanDefinition("name",nameBdf);
+        //通过bean名获取BeanDefinition
+        System.out.println(factory.getBeanDefinition("name"));
+        //通过名称判断是否注册过BeanDefinition
+        System.out.println(factory.containsBeanDefinition("name"));
+        
 
     }
 }
